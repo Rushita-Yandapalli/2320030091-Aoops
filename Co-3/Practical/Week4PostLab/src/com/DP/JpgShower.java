351de218aba6ae8a36aea479de646a50ee52ee45
Y.S.G.S RUSHITA <y.rushita1108@gmail.com>
2024-09-09 22:22:04 +0530

    Add files via upload
    PostLab

---
+++ Co-3/Practical/Week4PostLab/src/com/DP/JpgShower.java
@@ -0,0 +1,17 @@
+package com.DP;
+
+public class JpgShower implements AdvancedImageViewer{
+
+	@Override
+	public void showPNG() {
+		// TODO Auto-generated method stub
+		System.out.println("PNG image in JPG class");
+	}
+
+	@Override
+	public void showJPG() {
+		// TODO Auto-generated method stub
+		System.out.println("JPG image in JPG class");
+	}
+
+}
