d0bc5356fdfe038afbf5334dc3f849b95538d633
Y.S.G.S RUSHITA <y.rushita1108@gmail.com>
2024-09-14 13:24:29 +0530

    Add files via upload

---
+++ Co-1/Project/Project1and2/src/com/DP/Projector.java
@@ -0,0 +1,13 @@
+package com.DP;
+
+public class Projector {
+public void on() {
+	System.out.println("Projector is turned on.");
+}
+public void off() {
+	System.out.println("Projector is turned off.");
+}
+public void setInput(String input) {
+	System.out.println("Projector input set to" + input + ".");
+}
+}
